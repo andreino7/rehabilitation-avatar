ffaa83c469b40fc15a2a7a8523e485a68a4f1ccb
Mastercava <mastercava@hotmail.it>
2015-11-03 17:08:58 -0600

    Simple GUI + RPC Random

--- Assets/Scripts/ObjectGenerator.cs
+++ Assets/Scripts/ObjectGenerator.cs
@@ -9,6 +9,7 @@ public class ObjectGenerator : MonoBehaviour {
 	public float yOffset = 1f, verticalBounds = 1f, horizontalBounds = 1f;
 	private static ObjectGenerator instance;
 	private int currentObject = 0;
+	private float xAvatarSize = 0.5f;
 
 	private ObjectGenerator () {}
 
@@ -25,16 +26,40 @@ public class ObjectGenerator : MonoBehaviour {
 		CreateNewObject ();
 	}
 
-	public void CreateNewObject () {
-		currentObject++;
-		Vector3 newPosition = new Vector3 (Random.Range(-horizontalBounds, horizontalBounds), yOffset + Random.Range(-verticalBounds, verticalBounds), transform.position.z);
-		Quaternion newQuaternion = Quaternion.Euler (Random.Range (0f, 360f), Random.Range (0.0f, 360f), Random.Range (0.0f, 360f));   
-		newQuaternion = Quaternion.identity;
+	public void CreateNewObject() {
+		if(getReal3D.Cluster.isMaster) {
+			currentObject++;
+			if (currentObject == numberOfObjects) {
+				EndSession();
+				return;
+			}
+			Vector3 newPosition = new Vector3 (Random.Range(-horizontalBounds, horizontalBounds), yOffset + Random.Range(-verticalBounds, verticalBounds), transform.position.z + 0.5f);
+			if(Mathf.Abs(newPosition.x) < xAvatarSize) {
+				if (newPosition.x > 0)
+					newPosition.x = newPosition.x + xAvatarSize;
+				else if(newPosition.x < 0)
+					newPosition.x = newPosition.x - xAvatarSize;
+			}
+			Quaternion newQuaternion = Quaternion.Euler (Random.Range (0f, 360f), Random.Range (0.0f, 360f), Random.Range (0.0f, 360f));
+			getReal3D.RpcManager.call("CreateNewObjectRPC", newPosition, newQuaternion);
+		}
+	}
+
+	[getReal3D.RPC]
+	private void CreateNewObjectRPC (Vector3 newPosition, Quaternion newQuaternion) {
 		Instantiate (objectPrefab, newPosition, newQuaternion);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	private void EndSession() {
+		Invoke ("ChangeScene",3f);
+	}
+
+	private void ChangeScene() {
+		Application.LoadLevel ("Results");
 	}
 }
