da5fd595909ca17ea6cab24aae071521c62c9c49
Mastercava <mastercava@hotmail.it>
2015-11-03 15:27:30 -0600

    Initial Scene + RPC Call

--- Assets/Scripts/TrainingSelector.cs
+++ Assets/Scripts/TrainingSelector.cs
@@ -4,13 +4,27 @@ using System.Collections;
 
 public class TrainingSelector : MonoBehaviour {
 
+	public GameObject firstPanel, secondPanel;
 	public Text patientId;
 
+	private int trainingModeId;
+
 	public void InsertPatientId() {
-		if(patientId.text != "") {
-			PlayerPrefs.SetString("PatientId", patientId.text);
-			Application.LoadLevel("Main");
+		if(patientId.text != "" && getReal3D.Cluster.isMaster) {
+			firstPanel.SetActive(false);
+			secondPanel.SetActive(true);
 		}
 	}
 
+	public void SelectTrainingMode(int mode) {
+		trainingModeId = mode;
+		getReal3D.RpcManager.call("StartTraining");
+	}
+
+	[getReal3D.RPC]
+	private void StartTraining() {
+		PlayerPrefs.SetString("PatientId", patientId.text);
+		PlayerPrefs.SetInt("TrainingModeId", trainingModeId);
+		Application.LoadLevel("Main");
+	}
 }
